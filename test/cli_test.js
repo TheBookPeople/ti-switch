1912a9ab4c4a72941e9a3cebc3ef05637a079033
William Griffiths <william.griffiths@thebookpeople.co.uk>
2015-03-11 08:07:48 +0000

    check for reserved environment names

--- test/cli_test.js
+++ test/cli_test.js
@@ -24,5 +24,75 @@ describe('cli', function(){
 	  
 	  app_processor.run.restore();
     });
+	
+    it('should fail with xml as enviroment', function(){
+	  var stub = sinon.stub(app_processor, "run");
+	  
+	  cli.run(['node', '/usr/local/bin/ti-switch', 'xml']);
+	  
+	  assert.ok(!stub.called, ' app_processor should not have been called');
+	  
+	  app_processor.run.restore();
+    });
+	
+    it('should fail with js as enviroment', function(){
+	  var stub = sinon.stub(app_processor, "run");
+	  
+	  cli.run(['node', '/usr/local/bin/ti-switch', 'js']);
+	  
+	  assert.ok(!stub.called, ' app_processor should not have been called');
+	  
+	  app_processor.run.restore();
+    });
+	
+    it('should fail with json as enviroment', function(){
+	  var stub = sinon.stub(app_processor, "run");
+	  
+	  cli.run(['node', '/usr/local/bin/ti-switch', 'json']);
+	  
+	  assert.ok(!stub.called, ' app_processor should not have been called');
+	  
+	  app_processor.run.restore();
+    });
+	
+    it('should fail with txt as enviroment', function(){
+	  var stub = sinon.stub(app_processor, "run");
+	  
+	  cli.run(['node', '/usr/local/bin/ti-switch', 'txt']);
+	  
+	  assert.ok(!stub.called, ' app_processor should not have been called');
+	  
+	  app_processor.run.restore();
+    });
+	
+    it('should fail with png as enviroment', function(){
+	  var stub = sinon.stub(app_processor, "run");
+	  
+	  cli.run(['node', '/usr/local/bin/ti-switch', 'png']);
+	  
+	  assert.ok(!stub.called, ' app_processor should not have been called');
+	  
+	  app_processor.run.restore();
+    });
+	
+    it('should fail with mst as enviroment', function(){
+	  var stub = sinon.stub(app_processor, "run");
+	  
+	  cli.run(['node', '/usr/local/bin/ti-switch', 'mst']);
+	  
+	  assert.ok(!stub.called, ' app_processor should not have been called');
+	  
+	  app_processor.run.restore();
+    });
+	
+    it('should fail with XML as enviroment', function(){
+	  var stub = sinon.stub(app_processor, "run");
+	  
+	  cli.run(['node', '/usr/local/bin/ti-switch', 'XML']);
+	  
+	  assert.ok(!stub.called, ' app_processor should not have been called');
+	  
+	  app_processor.run.restore();
+    });
   });
 });
